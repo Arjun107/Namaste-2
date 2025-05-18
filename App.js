0a64a10805890f74ab3b27df4e40e4453bfd570f
Arjun107 <arshdawra7@gmail.com>
2025-05-19 00:38:02 +0530

    second commit

--- App.js
+++ App.js
@@ -1,15 +1,20 @@
-const heading = React.createElement("div", { id: "parent" },
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+
+const parent = React.createElement("div", { id: "parent" },
     [React.createElement("div", { id: "child" },
-        [React.createElement("h1", {}, "I am h1 tag"),
-        React.createElement("h2", {}, "I am h2 tag")
+        [React.createElement("h1", {}, "Thiis is react project"),
+        React.createElement("h2", {}, "by arsh dawra")
     ]),
-    React.createElement("div", { id: "child 2" },
+    React.createElement("div", { id: "child2" },
         [React.createElement("h1", {}, "I am h1 tag"),
         React.createElement("h2", {}, "I am h2 tag")
     ])
 ]);
 
+console.log(parent);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(heading);
+root.render(parent);
