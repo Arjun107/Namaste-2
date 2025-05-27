d42b623de79ef796fc4464e109ac90a53bebed00
Arjun107 <arshdawra7@gmail.com>
2025-05-27 08:47:39 +0530

    devlopment

--- App.js
+++ App.js
@@ -10,7 +10,7 @@ import ReactDOM from "react-dom/client";
  * - ResturantContainer
  * - ResturantCard
  *   - img
- *   - Name of res, star rating , cusine , 
+ *   - Name of res, star rating , cusine ,
  * Footer
  * - Copy
  * - Links
@@ -22,7 +22,10 @@ const Header = () => {
   return (
     <div className="header">
       <div className="logo-container">
-        <img className="logo" src="https://media.istockphoto.com/id/1435983029/vector/food-delivery-logo-images.jpg?s=612x612&w=0&k=20&c=HXPxcjOxUiW4pMW1u9E0k2dJYQOU37a_0qZAy3so8fY=" />
+        <img
+          className="logo"
+          src="https://media.istockphoto.com/id/1435983029/vector/food-delivery-logo-images.jpg?s=612x612&w=0&k=20&c=HXPxcjOxUiW4pMW1u9E0k2dJYQOU37a_0qZAy3so8fY="
+        />
       </div>
       <div className="nav-items">
         <ul>
@@ -36,33 +39,47 @@ const Header = () => {
   );
 };
 
-const ResturantCard = () =>{
-  return(
-    <div className="res-card">
-      <h3>Mcdonlods</h3>
-    </div>
-  )
-}
 
-const Body = () =>{
-  return(
+
+const ResturantCard = () => {
+  return (
+    <div className="res-card" style={{backgroundColor: "#f0f0f0"}}>
+      <img
+        className="res-logo"
+        alt="res-logo"
+        src="https://www.shutterstock.com/image-photo/ayutthayathailand-03-2023-double-mcdonalds-260nw-2326307867.jpg"
+      ></img>
+      <h3>Mcdonlods</h3>
+      <h4>Biryani , North Indian , Asian</h4>
+      <h4>4.4 starts</h4>
+      <h4>20 minutes</h4>
+    </div>
+  );
+};
+
+const Body = () => {
+  return (
     <div className="body">
-      <div className="search">
-        Search
-      </div>
+      <div className="search">Search</div>
       <div className="res-container">
-       <ResturantCard/>
+        <ResturantCard />
+        <ResturantCard />
+        <ResturantCard />
+        <ResturantCard />
+        <ResturantCard />
+        <ResturantCard />
       </div>
     </div>
-  )
-}
+  );
+};
 
 const AppLayout = () => {
-  return <div className="app">
-    <Header/>
-    <Body/>
-
-  </div>;
+  return (
+    <div className="app">
+      <Header />
+      <Body />
+    </div>
+  );
 };
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
